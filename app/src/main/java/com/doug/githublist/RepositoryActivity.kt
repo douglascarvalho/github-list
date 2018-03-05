0dc15ad25ab0bc83804128a0b195471a2c60ca1a
Douglas Carvalho <douglas.carvalho@concrete.com.br>
2018-03-05 15:16:31 -0300

    Rename MainActivity to RepositoryActivity

--- app/src/main/java/com/doug/githublist/RepositoryActivity.kt
+++ app/src/main/java/com/doug/githublist/RepositoryActivity.kt
@@ -3,10 +3,10 @@ package com.doug.githublist
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class RepositoryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_repository)
     }
 }
